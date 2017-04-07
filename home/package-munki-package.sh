7c93b21fa1c8681cd98e3801d7155bdc9136ccf2
Brynnen Owen <owen@illinois.edu>
2017-04-06 19:06:03 -0500

    Fixed packaging file to include reconfigure pieces.

--- home/package-munki-package.sh
+++ home/package-munki-package.sh
@@ -12,9 +12,11 @@ mkdir "$GLOBAL_PACKAGEPATH"/munkiconfig-out/munkiconfig-$VERSION.pkg
 # This makes the package linux-style.  Google tells you how to do this.
 cd "$GLOBAL_PACKAGEPATH"/Contents
 find . | cpio -o --format odc --owner 0:80 |gzip -c > "$GLOBAL_PACKAGEPATH"/munkiconfig-out/munkiconfig-$VERSION.pkg/Payload
+cd "$GLOBAL_PACKAGEPATH"/Scripts
+find . | cpio -o --format odc --owner 0:80 |gzip -c > "$GLOBAL_PACKAGEPATH"/munkiconfig-out/munkiconfig-$VERSION.pkg/Scripts
 mkbom -u 0 -g 80 . "$GLOBAL_PACKAGEPATH"/munkiconfig-out/munkiconfig-$VERSION.pkg/Bom
 
-cat - > "$GLOBAL_PACKAGEPATH/munkiconfig-out/munkiconfig-$VERSION.pkg/Distribution" <<EOF
+cat - > "$GLOBAL_PACKAGEPATH/munkiconfig-out/Distribution" <<EOF
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <installer-script authoringTool="edu.illinois.PackageMaker" authoringToolVersion="3.0.4" authoringToolBuild="179" minSpecVersion="1">
     <title>MunkiConfig - Configure Munki for Illinois</title>
@@ -24,11 +26,27 @@ cat - > "$GLOBAL_PACKAGEPATH/munkiconfig-out/munkiconfig-$VERSION.pkg/Distributi
         <line choice="config"/>
     </choices-outline>
     <choice id="config" title="Munki configuration"
-	    description="Configures Munki to use the shared Munki Service">
+            description="Configures Munki to use the shared Munki Service">
         <pkg-ref id="edu.illinois.munkiconfig"/>
     </choice>
     <pkg-ref id="edu.illinois.munkiconfig" installKBytes="3" version="$VERSION" auth="Root">#munkiconfig-$VERSION.pkg</pkg-ref>
 </installer-script>
+EOF
+
+cat - > "$GLOBAL_PACKAGEPATH/munkiconfig-out/munkiconfig-$VERSION.pkg/PackageInfo" <<EOF
+<?xml version="1.0" standalone="no"?>
+<pkg-info format-version="2" install-location="/" identifier="edu.illinois.munkiconfig" version="$VERSION" generator-version="InstallCmds-502 (14E46)" auth="root">
+    <payload numberOfFiles="9" installKBytes="4"/>
+    <bundle-version/>
+    <upgrade-bundle/>
+    <update-bundle/>
+    <atomic-update-bundle/>
+    <strict-identifier/>
+    <relocate/>
+    <scripts>
+      <postinstall file="./postflight"/>
+    </scripts>
+</pkg-info>
 EOF
 
 cd "$GLOBAL_PACKAGEPATH"/munkiconfig-out
