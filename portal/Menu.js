6b009e784dc89af77af136c28d6bd6f426d79ab7
owen@illinois.edu <owen@illinois.edu>
2022-12-08 10:40:34 -0600

    Clean up unused Menu.js code

--- portal/Menu.js
+++ portal/Menu.js
@@ -7,16 +7,6 @@ app.controller('menuController',function($scope,$location,$window,Authentication
 	$scope.username = data;
     });
 
-    <!-- // A 'perm' category is for future expansion where only some items show up.
-    var rawnavitems = [{url:'#/computers/search',name:'Manage Computers',location:'L',perm:['']},
-                       {url:'#/usergroups/search',name:'Manage Groups and Permissions',location:'L',perm:['']},
-                       {url:'#/repositories/search',name:'Manage Repositories',location:'L',perm:['']},
-                       {url:'#/downloads',name:'Downloads',location:'L',perm:['']},
-                       {url:'https://munkireport.eps.uillinois.edu',name:'Reporting',location:'L',perm:['']},
-                       {url:'https://answers.uillinois.edu/search.php?q=munki+-FAA',name:'Documentation',location:'L',perm:['']},
-                       {url:'packages.html',name:'Available Software',location:'L',perm:['']},
-                      ];
-    -->
     // A 'perm' category is for future expansion where only some items show up.
     // A 'target' category controls whether the nav bar item opens in the same or a new window/tab in the browser
     var rawnavitems = [{url:'#/computers/search',name:'Manage Computers',location:'L',perm:[''],target:'_self'},
